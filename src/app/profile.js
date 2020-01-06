1e8066bf19802d80cb47e28f15aec665363a069a
Lucas Homer <lucashomer@lucass-mbp.home>
2020-01-05 21:09:29 -0800

    invoke fetch from db via lambda in profile page after user has logged in via identity

--- src/app/profile.js
+++ src/app/profile.js
@@ -1,8 +1,20 @@
 import React from "react"
 
 import { useIdentityContext } from "react-netlify-identity-widget"
+
 const Profile = () => {
   const { user } = useIdentityContext()
+
+  React.useEffect(() => {
+    if (!user) return
+    async function fetchTodos() {
+      const response = await fetch("/.netlify/functions/todos")
+      const data = await response.json()
+
+      console.log(data)
+    }
+    fetchTodos()
+  }, [user])
   return (
     <>
       <h1>Your profile</h1>
